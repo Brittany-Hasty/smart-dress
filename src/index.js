402df8222c180b103a78700951921e8db8c119df
Brittany Hasty <Brittany.R.Hasty@gmail.com>
2020-11-13 23:41:24 -0800

    Add error handler

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { getDressIdeas } = require('./utility/getDressIdeas');
+const port = process.env.PORT || 3000;
+const app = express();
+const { urlWordExtractorRegex } = require('./utility/regex/urlWordExtractor');
+
+app.get('/outfits/*', async (req, res) => {
+    const matchResults = urlWordExtractorRegex.exec(req.originalUrl);
+    if (!matchResults) {
+        throw new Error("Could not extract url");
+    }
+    let word = matchResults[1];
+    if (!word || word === ""){
+       word = "fantastic"; 
+    }
+    const dressIdeas = await getDressIdeas(word);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(dressIdeas, null, 2));
+})
+
+app.use((error, req, res, next) => {
+    // Bad request error
+    res.status(400)
+    res.json( /* ... */ )
+})
+
+app.listen(port);
